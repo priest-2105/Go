271c04471b47e2aac94dc052425135a40acfb1fe
priest-2105 <priest2105@gmail.com>
2025-08-25 14:25:20 +0100

    constants

--- main.go
+++ main.go
@@ -145,7 +145,29 @@ import (
 // 	fmt.Printf("%v, %T\n", n, n)
 // } 
 
-func main (){
-	s := "This is a string"
-	fmt.Printf("%v, %T\n", string(s[2]), s[2])
-}
+// func main (){
+// 	s := "This is a string"
+// 	fmt.Printf("%v, %T\n", string(s[2]), s[2])
+// }
+
+
+// func main(){
+// 	    const myConst int = 42
+// 		fmt.Printf("%v, %T\n", myConst, myConst)
+// }
+
+// const a int16 = 27
+
+// func main() {
+// 	const a int = 14
+// 	const b string = "foo"
+// 	const c float32 = 3.14
+// 	const d bool = true
+	
+//     fmt.Printf("%v\n", a)
+//     fmt.Printf("%v\n", b)
+//     fmt.Printf("%v\n", c)
+//     fmt.Printf("%v\n", d)
+// }
+
+
