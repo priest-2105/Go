2c44c06bdcfa89913dccc8114f7d8c0b9fcb9e70
priest-2105 <priest2105@gmail.com>
2025-08-29 21:58:08 +0100

    loops

--- main.go
+++ main.go
@@ -361,15 +361,23 @@ import (
 // }
 
 
+// func main () {
+// 	switch 5 {
+// 	case 1, 5, 10:
+// 		fmt.Println("one, five or ten")
+	
+// 	case 2, 4, 6:
+// 		fmt.Println("two, four or six")
+	
+// 	default:
+// 		fmt.Println("another number")
+// 	}
+// }
+
 func main () {
-	switch 5 {
-	case 1, 5, 10:
-		fmt.Println("one, five or ten")
-	
-	case 2, 4, 6:
-		fmt.Println("two, four or six")
-	
-	default:
-		fmt.Println("another number")
+
+	for i, j := 0, 0; i < 5; i, j = i+1, j+1 {
+		 fmt.Println(i, j)
 	}
+
 }
