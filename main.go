47c8f206a92dc1904b3bda5d6e9bc5a37dbf9d47
priest-2105 <priest2105@gmail.com>
2025-09-01 01:02:21 +0100

    pointers

--- main.go
+++ main.go
@@ -4,7 +4,7 @@ import (
 	// "fmt"
 	// "io/ioutil"
 	// "log"
-	"net/http"
+	"fmt"
 )
 
 // var i float32 = 42
@@ -443,12 +443,28 @@ import (
 // 	fmt.Println("end")
 // }
 
-func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-         w.Write([]byte("Hello Go !"))
-	})
-	err :=http.ListenAndServe(":8080", nil)
-	if err != nil {
-		panic(err.Error())
-	}
+// func main() {
+// 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+//          w.Write([]byte("Hello Go !"))
+// 	})
+// 	err :=http.ListenAndServe(":8080", nil)
+// 	if err != nil {
+// 		panic(err.Error())
+// 	}
+// }
+
+
+// func main(){
+// 	var a int = 42
+// 	var b *int = &a
+// 	fmt.Println(a, *b)
+// 	a = 27
+// 	fmt.Println(a, *b)
+// }
+
+func main(){	
+	a := [3]int{1, 2, 3}
+	b := &a[0]
+	c := &a[1]
+	fmt.Printf("%v %p %p\n", a, b, c)
 }
