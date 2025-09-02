959cd477cf3faa5a529d6f07a1c165f0255b22d1
priest-2105 <priest2105@gmail.com>
2025-09-02 23:29:42 +0100

    interfaces

--- main.go
+++ main.go
@@ -491,10 +491,29 @@ import (
 
 
 
+// func main() {
+// 	sayGreeting("Hello", "Stacey")
+// }
+
+// func sayGreeting(greeting, name string) {
+// 	fmt.Println(greeting, name)
+// }
+
+
 func main() {
-	sayGreeting("Hello", "Stacey")
+	var w Writer = ConsoleWriter{}
+	w.Write([]byte("Hello Go !"))
 }
 
-func sayGreeting(greeting, name string) {
-	fmt.Println(greeting, name)
+type Writer interface {
+	Write([]byte) (int, error)
 }
+
+type ConsoleWriter struct {}
+
+func(cw ConsoleWriter) Write(data []byte) (int, error) {
+	n, err := fmt.Println((string(data)))
+	return n, err
+}
+
+
